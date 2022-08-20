b8ef5471136974777187bc29423afd945565bef2
Jan <hanohj@gmail.com>
2022-08-20 20:28:28 +0300

    Added another layer of verification

--- src/main/java/com/corporate/onlinesponsoredads/converter/CampaignConverter.java
+++ src/main/java/com/corporate/onlinesponsoredads/converter/CampaignConverter.java
@@ -6,6 +6,8 @@ import com.corporate.onlinesponsoredads.entity.ProductEntity;
 import com.corporate.onlinesponsoredads.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,19 +19,23 @@ public class CampaignConverter {
 
     public CampaignEntity convertDTOtoEntity(CampaignDTO campaignDTO) {
         // Below is Adapter Design Pattern
-        CampaignEntity pe = new CampaignEntity();
-        pe.setStartingDate(campaignDTO.getStartingDate());
-        pe.setName(campaignDTO.getName());
-        pe.setBid(campaignDTO.getBid());
-        pe.setProducts(campaignDTO.getProducts());
-        // find all entities from this list
-        List<Long> parsedLongs = Arrays.asList(campaignDTO.getProducts().split(",")).stream().map(s -> Long.parseLong(s.trim())).collect(Collectors.toList());
-        List<ProductEntity> productsList = (List<ProductEntity>) productRepository.findAllById(parsedLongs);
-        List<Long> filtered = productsList.stream().map(prod -> prod.getId()).collect(Collectors.toList());
-        // set the entities
-        String joinedFiltered = filtered.stream().map(String::valueOf).collect(Collectors.joining(","));
-        pe.setProducts(joinedFiltered);
-        return pe;
+        CampaignEntity campaginEntity = new CampaignEntity();
+        campaginEntity.setStartingDate(campaignDTO.getStartingDate());
+        campaginEntity.setName(campaignDTO.getName());
+        campaginEntity.setBid(campaignDTO.getBid());
+
+        String commaSeparated = campaignDTO.getProducts();
+        if(commaSeparated != null && !commaSeparated.trim().isEmpty()) {
+            // find all entities from this list
+            List<Long> parsedLongs = Arrays.asList(commaSeparated.split(",")).stream().map(s -> Long.parseLong(s.trim())).collect(Collectors.toList());
+            List<ProductEntity> productsList = (List<ProductEntity>) productRepository.findAllById(parsedLongs);
+            List<Long> filtered = productsList.stream().map(prod -> prod.getId()).collect(Collectors.toList());
+            // set the entities
+            String joinedFiltered = filtered.stream().map(String::valueOf).collect(Collectors.joining(","));
+            campaginEntity.setProducts(joinedFiltered);
+            return campaginEntity;
+        }
+        return campaginEntity;
     }
 
         public CampaignDTO convertEntitytoDTO(CampaignEntity campaignEntity) {
