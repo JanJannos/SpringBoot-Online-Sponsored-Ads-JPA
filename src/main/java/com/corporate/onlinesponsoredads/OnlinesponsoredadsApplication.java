21581d32fff97b3d4cd6d441eb52200fb40f9707
Jan <hanohj@gmail.com>
2022-08-20 15:24:48 +0300

    First Commit

---
+++ src/main/java/com/corporate/onlinesponsoredads/OnlinesponsoredadsApplication.java
@@ -0,0 +1,13 @@
+package com.corporate.onlinesponsoredads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinesponsoredadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinesponsoredadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corporate/onlinesponsoredads/OnlinesponsoredadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corporate.onlinesponsoredads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinesponsoredadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
